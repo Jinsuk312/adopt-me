ed5efebfc7c7d8e114b312cc1eb6530bdc50ae17
jinsukmik <jinsukmik@gmail.com>
2019-03-14 21:00:41 -0500

    finished modal to details of pets

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -30755,7 +30755,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53763" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54827" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
